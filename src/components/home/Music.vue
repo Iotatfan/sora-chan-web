38dd3a84df965ab7287d188425fcee44ae8a9ae2
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2021-08-27 13:20:13 +0700

    add home image and deploy stuff

--- src/components/home/Music.vue
+++ src/components/home/Music.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-xxl content">
     <div class="d-flex flex-lg-row flex-column-reverse container">
-      <div class="d-flex flex-column m-3">
+      <div class="d-flex flex-column m-3 pe-5 pt-5">
         <div class="text-start">
           <!-- <a class="btn btn-discord"> Social </a> -->
           <h1 class="headline">Solve your music need</h1>
@@ -13,7 +13,12 @@
       </div>
       <div class="d-flex flex-column m-3">
         <div class="me-auto">
-          <h1 class="headline">Placeholder for Screenshots</h1>
+          <img
+            src="@/assets/music.svg"
+            alt="Solve your music need"
+            class="img-fluid"
+            height="512"
+          />
         </div>
       </div>
     </div>
--- src/components/home/Social.vue
+++ src/components/home/Social.vue
@@ -3,10 +3,15 @@
     <div class="d-flex flex-lg-row flex-column container">
       <div class="d-flex flex-column m-3">
         <div class="text-start">
-          <h1 class="headline">Placeholder for Screenshots</h1>
+          <img
+            src="@/assets/social.svg"
+            alt="Listen along with your friends"
+            class="img-fluid"
+            height="512"
+          />
         </div>
       </div>
-      <div class="d-flex flex-column m-3">
+      <div class="d-flex flex-column m-3 ps-5 pt-5">
         <div class="text-start">
           <!-- <a class="btn btn-discord"> Social </a> -->
           <h1 class="headline">Listen along with your friends</h1>
