a0598c01e3a8e251830dbf2b70c5e50573a4baee
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2022-01-28 19:27:56 +0700

    Updates

--- js/about.424758b5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"container-xxl"},s=Object(a["g"])("h1",null,"This is an about page",-1),o=[s];function i(n,t){return Object(a["s"])(),Object(a["f"])("div",c,o)}const u={};u.render=i;t["default"]=u}}]);
-//# sourceMappingURL=about.424758b5.js.map
--- js/about.7b0af156.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,e,t){"use strict";t.r(e);var a=t("7a23"),c={class:"container-xxl"},o=Object(a["f"])("h1",null,"This is an about page",-1),s=[o];function i(n,e){return Object(a["r"])(),Object(a["e"])("div",c,s)}const r={};r.render=i;e["default"]=r}}]);
-//# sourceMappingURL=about.7b0af156.js.map
